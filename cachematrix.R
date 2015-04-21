33aac6b51b6e8c091520c3363987f96b737c027f
Guangze <glosslee@gmail.com>
2015-04-20 23:42:05 -0400

    Homework 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## GL 2015/04/20
+## To solve and cache a inverse matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix is to cache the existing inverse matrix in oder to
+## speed up calculation
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function (y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function () x
+        setCacheMatrix <- function (matr) m <<- matr
+        getCacheMatrix <- function () m
+        
+        list (set = set, get = get, setCacheMatrix = setCacheMatrix, 
+              getCacheMatrix = getCacheMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: to either solve the matrix or determine it is in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getCacheMatrix
+        if (!is.null(m)) {
+                message ("getting cached data")
+                return m
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setCacheMatrix(m)
+        m
 }
